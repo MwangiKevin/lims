990e4d9705a43a4c57df399c7ac0a01d82a85df9
MwangiKevin <mwangikevinn@gmail.com>
2014-07-15 13:19:36 +0300

    Upgraded Highcharts/Highmaps created dashboard module
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

---
+++ application/modules/dashboard/controllers/dashboard.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
+
+class dashboard extends MY_Controller {
+
+	public function index(){
+		
+	}
+
+	public function load_dashboard(){
+
+	}
+
+	
+}
