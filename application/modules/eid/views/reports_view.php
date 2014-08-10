99206b9fc3b41f44cc5184899d938f34012efa47
MwangiKevin <mwangikevinn@gmail.com>
2014-08-11 01:43:01 +0300

    modified Reports Page
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/modules/eid/views/reports_view.php
+++ application/modules/eid/views/reports_view.php
@@ -4,231 +4,275 @@
 		<a class="pink" data-toggle="modal" href="#"> Select a date range to generate a report. </a>
 	</h4>
 	<div class="hr hr-18 hr-double dotted"></div>
-	
-	<div class="widget-header widget-header-flat" style="margin-bottom: 20px;">
-		<div class="widget-title smaller">
-			<strong>
-				<h2>Reports</h2>
-			</strong>
+
+	<div class="widget-box">
+
+		<div class="widget-header widget-header-flat widget-header-small">
+			<h5 class="widget-title">
+				<i class="ace-icon fa fa-list-alt"></i>
+				Reports
+			</h5>
 		</div>
-	</div>
-	
-	
-	<!--All samples report-->
-	<div class="col-sm-6">
-		<div class="widget-header widget-header-flat" >
-			<h4 class="widget-title smaller">
-				<i class="ace-icon fa fa-quote-left smaller-80"></i>
-				All Samples
-			</h4>
-		</div>
-		<form method="post" action="<?php echo base_url().'eid/reports/all_samples' ?>">
-			<div class="input-group my-input-group nb">
-				<span class="input-group-addon my-input-group-caption">
-					Start<br/>Date:
-				</span>
-				<input name="all_start_date" id="all_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-				<span class="input-group-addon my-input-group-icon">
-					<i class="ace-icon fa fa-calendar"></i>
-				</span>
-			</div>
-			<div class="input-group my-input-group nb">
-				<span class="input-group-addon my-input-group-caption">
-					End<br/>Date:
-				</span>
-				<input name="all_end_date" id="all_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-				<span class="input-group-addon my-input-group-icon">
-					<i class="ace-icon fa fa-calendar"></i>
-				</span>
-			</div>
-			<!-- Submit Button -->
-			<div style="position:absolute; right:20%; top:100%;">
-				<input type="submit" class="btn btn-primary" value="Generate Report">
-			</div>
-		</form>
-	</div>
-	<!--All samples report-->
-	
-	<!--Reports according to geographic area-->
-	<div class="col-sm-6">
-		<div class="widget-header widget-header-flat" >
-			<h4 class="widget-title smaller">
-				<i class="ace-icon fa fa-quote-left smaller-80"></i>
-				Report according to geographic area
-			</h4>
-		</div>
-		<div class="tabbable tabs-left">
-			<ul id="myTab3" class="nav nav-tabs">
-				<li class="active">
-					<a href="#home3" data-toggle="tab">
-							<i class="ace-icon fa fa-desktop"></i>
-							Region
-					</a>
-				</li>
-				<li class="">
-					<a href="#profile3" data-toggle="tab">
-						<i class="ace-icon fa fa-circle"></i>
-						District
-					</a>
-				</li>
-				<li>
-					<a href="#dropdown13" data-toggle="tab">
-						<i class="ace-icon fa fa-circle-o"></i>
-						Facility
-					</a>
-				</li>
-			</ul>
-			<div class="tab-content">
-			<!--Region-->
-				<div id="home3" class="tab-pane active">
-					<form action="<?php echo base_url().'eid/reports/reports_by_area/0' ?>" method="POST">
-						<!-- Select a Region-->
-						<div class="input-group my-input-group nb" style="width:100%">
-							<span class="input-group-addon my-input-group-caption">
-								Region
-							</span>
-							<Select name="region" id="region" style="width:76%" >
-								<option value=""> Select a Region </option>
-								<?php 
-								foreach ($regions as $key => $value) {
-									if( empty($value["id"])){
-								?>
-									<option value="-1"> No Data</option>
-								<?php	
-									}else{
-								?>
-									<option value="<?php echo $value["id"].".".$value["name"]?>"><?php echo $value["name"]?></option>
-								<?php 
-									}
-								}
-								?>								
-							</Select> 
-						</div>
-						<!-- Select a Region-->
-						<div class="input-group my-input-group nb">
-							<span class="input-group-addon my-input-group-caption">
-								Start<br/>Date:
-							</span>
-							<input name="region_start_date" id="region_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-							<span class="input-group-addon my-input-group-icon">
-								<i class="ace-icon fa fa-calendar"></i>
-							</span>
-						</div>
-						<div class="input-group my-input-group nb">
-							<span class="input-group-addon my-input-group-caption">
-								End<br/>Date:
-							</span>
-							<input name="region_end_date" id="region_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-							<span class="input-group-addon my-input-group-icon">
-								<i class="ace-icon fa fa-calendar"></i>
-							</span>
-						</div>
-						<i class="ace-icon fa fa-desktop" style="position: absolute; right: 10px; top:20px;"></i>
-						<div style="margin-left:70%;">
-							<input type="submit" name = "region_generate_report" id="region_generate_report" class="btn btn-primary"  value="Generate Report" />
-						</div>
-					</form>
+
+		<div class="widget-body">
+			<div class="widget-main">
+				<h3 class="header blue lighter smaller">
+					<i class="ace-icon fa fa-bars smaller-90"></i>
+					National Reports
+				</h3>
+
+				<div id ="" class ="reports_spaced" style="">
+					<table class="my-infobox infobox">
+						<form method="post" action="<?php echo base_url().'eid/reports/all_samples' ?>">
+							<tr style = "">
+								<td style="vertical-align:middle;">
+									<div style="float:right;padding-right:15%;" >
+										National
+									</div>
+								</td>
+								<td>
+									<div class="input-group my-input-group reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Start<br/>Date:
+										</span>
+										<input name="all_start_date" id="all_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>
+								</td>
+								<td>									
+									<div class="input-group my-input-group reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											End<br/>Date:
+										</span>
+										<input name="all_end_date" id="all_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>
+								</td>
+								<td>									
+									<div class="input-group my-input-group reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Report Type
+										</span>
+										<select name="all_type" id="all_type" >
+											<option value="1">Detailed</option>
+											<option value="2">Summary</option>
+										</select>
+									</div>
+								</td>
+								<td>									
+									<div style="">
+										<input type="submit" class="btn btn-primary btn-sm" value="Generate Report">
+									</div>
+								</td>
+							</tr>
+						</form>
+						<tr style="height:10px;">
+						</tr>
+						<form method="post" action="<?php echo base_url().'eid/reports/reports_by_area/0';?>">
+							<tr>
+								<td>									
+									<div class="input-group my-input-group nb" style="width:100%">
+										<span class="input-group-addon my-input-group-caption">
+											Region
+										</span>
+										<Select name="region" id="region" style="width:76%" >
+											<option value=""> Select a Region </option>
+											<?php 
+											foreach ($regions as $key => $value) {
+												if( empty($value["id"])){
+													?>
+													<option value="-1"> No Data</option>
+													<?php	
+												}else{
+													?>
+													<option value="<?php echo $value["id"].".".$value["name"]?>"><?php echo $value["name"]?></option>
+													<?php 
+												}
+											}
+											?>								
+										</Select> 
+									</div>
+								</td>
+								<td>
+									<div class="input-group my-input-group  reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Start<br/>Date:
+										</span>
+										<input name="region_start_date" id="region_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>	
+								</td>
+								<td>									
+									<div class="input-group my-input-group  reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											End<br/>Date:
+										</span>
+										<input name="region_end_date" id="region_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>
+								</td>
+								<td>																	
+									<div class="input-group my-input-group reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Report Type
+										</span>
+										<select name="region_type" id="region_type" >
+											<option value="1">Detailed</option>
+											<option value="2">Summary</option>
+										</select>
+									</div>
+								</td>
+								<td>
+									<div >
+										<input type="submit" name = "region_generate_report" id="region_generate_report" class="btn btn-primary btn-sm"  value="Generate Report" />
+									</div>
+								</td>
+							</tr>
+						</form>
+
+						<form method="post" action="<?php echo base_url().'eid/reports/reports_by_area/1';?>">
+							<tr>
+								<td>									
+									<div class="input-group my-input-group nb" style="width:100%">
+										<span class="input-group-addon my-input-group-caption">
+											District
+										</span>
+										<Select name="district" id="district" style="width:76%"   required >
+											<option value=""> Select a District </option>
+											<?php 
+											foreach ($districts as $key => $value) {
+												?>
+												<option value="<?php echo $value["id"].".".$value["name"]?>"><?php echo $value["name"]?></option>
+												<?php 
+											}
+											?>
+											<option value="-1"> No Data</option>
+										</Select> 
+									</div>
+								</td>
+								<td>
+									<div class="input-group my-input-group  reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Start<br/>Date:
+										</span>
+										<input name="district_start_date" id="district_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>	
+								</td>
+								<td>									
+									<div class="input-group my-input-group  reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											End<br/>Date:
+										</span>
+										<input name="district_end_date" id="district_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>
+								</td>
+								<td>																	
+									<div class="input-group my-input-group reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Report Type
+										</span>
+										<select name="district_type" id="district_type" >
+											<option value="1">Detailed</option>
+											<option value="2">Summary</option>
+										</select>
+									</div>
+								</td>
+								<td>
+									<div >
+										<input type="submit" name = "district_generate_report" id="district_generate_report" class="btn btn-primary btn-sm"  value="Generate Report" />
+									</div>
+								</td>
+							</tr>
+						</form>
+
+						<form method="post" action="<?php echo base_url().'eid/reports/reports_by_area/2';?>">
+							<tr>
+								<td>									
+									<div class="input-group my-input-group nb" style="width:100%">
+										<span class="input-group-addon my-input-group-caption">
+											Facility
+										</span>
+										<Select name="facility" id="facility" style="width:76%"   required >
+											<option value=""> Select a Facility </option>
+											<?php 
+											foreach ($facility as $key => $value) {
+												?>
+												<option value="<?php echo $value["id"].".".$value["name"]?>"><?php echo $value["name"]?></option>
+												<?php 
+											}
+											?>
+											<option value="-1"> No Data</option>
+										</Select> 
+									</div>
+								</td>
+								<td>
+									<div class="input-group my-input-group  reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Start<br/>Date:
+										</span>
+										<input name="facility_start_date" id="facility_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>	
+								</td>
+								<td>									
+									<div class="input-group my-input-group  reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											End<br/>Date:
+										</span>
+										<input name="facility_end_date" id="facility_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
+										<span class="input-group-addon my-input-group-icon">
+											<i class="ace-icon fa fa-calendar"></i>
+										</span>
+									</div>
+								</td>
+								<td>																	
+									<div class="input-group my-input-group reports_spaced">
+										<span class="input-group-addon my-input-group-caption">
+											Report Type
+										</span>
+										<select name="facility_type" id="facility_type" >
+											<option value="1">Detailed</option>
+											<option value="2">Summary</option>
+										</select>
+									</div>
+								</td>
+								<td>
+									<div >
+										<input type="submit" name = "district_generate_report" id="district_generate_report" class="btn btn-primary btn-sm"  value="Generate Report" />
+									</div>
+								</td>
+							</tr>
+						</form>
+					</table>
 				</div>
-				<!--Region-->
+				<div class="hr hr8 hr-double"></div>
+
+				<h3 class="header blue lighter smaller">
+					<i class="ace-icon fa fa-bars smaller-90"></i>
+					Lab Reports
+				</h3>
 				
-				<!--District-->
-				<div id="profile3" class="tab-pane">
-					<form action="<?php echo base_url().'eid/reports/reports_by_area/1' ?>" method="POST" >
-						<!--Select a District-->
-						<div class="input-group my-input-group nb" style="width:100%">
-							<span class="input-group-addon my-input-group-caption">
-								District
-							</span>
-							<Select name="district" id="district" style="width:76%"   required >
-								<option value=""> Select a District </option>
-								<?php 
-								foreach ($districts as $key => $value) {
-									?>
-									<option value="<?php echo $value["id"].".".$value["name"]?>"><?php echo $value["name"]?></option>
-									<?php 
-								}
-								?>
-								<option value="-1"> No Data</option>
-							</Select> 
-						</div>
-						<!--Select a District-->
-						
-						<div class="input-group my-input-group nb">
-							<span class="input-group-addon my-input-group-caption">
-								Start<br/>Date:
-							</span>
-							<input name="district_start_date" id="district_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-							<span class="input-group-addon my-input-group-icon">
-								<i class="ace-icon fa fa-calendar"></i>
-							</span>
-						</div>
-						<div class="input-group my-input-group nb">
-							<span class="input-group-addon my-input-group-caption">
-								End<br/>Date:
-							</span>
-							<input name="district_end_date" id="district_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-							<span class="input-group-addon my-input-group-icon">
-								<i class="ace-icon fa fa-calendar"></i>
-							</span>
-						</div>
-						<i class="ace-icon fa fa-circle" style="position: absolute; right: 10px; top:20px;"></i>
-						<div style="margin-left:70%;">
-							<input type="submit" id="district_generate_report" name="district_generate_report" value="Generate Report" class="btn btn-primary" />
-						</div>
-					</form>
-				</div>
-				<!--District-->
-				<!--Facility-->
-				<div id="dropdown13" class="tab-pane">
-					<form action="<?php echo base_url().'eid/reports/reports_by_area/2' ?>" method="POST" >
-						
-						<!-- Select Facility -->
-						<div class="input-group my-input-group nb" style="width:100%">
-							<span class="input-group-addon my-input-group-caption">
-								Facility
-							</span>
-							<Select name="facility" id="facility" style="width:76%"   required >
-								<option value=""> Select a Facility </option>
-								<?php 
-								foreach ($facility as $key => $value) {
-									?>
-									<option value="<?php echo $value["id"].".".$value["name"]?>"><?php echo $value["name"]?></option>
-									<?php 
-								}
-								?>
-								<option value="-1"> No Data</option>
-							</Select> 
-						</div>
-						<!-- Select Facility-->
-						
-						<div class="input-group my-input-group nb">
-							<span class="input-group-addon my-input-group-caption">
-								Start<br/>Date:
-							</span>
-							<input name="facility_start_date" id="facility_start_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-							<span class="input-group-addon my-input-group-icon">
-								<i class="ace-icon fa fa-calendar"></i>
-							</span>
-						</div>
-						<div class="input-group my-input-group nb">
-							<span class="input-group-addon my-input-group-caption">
-								End<br/>Date:
-							</span>
-							<input name="facility_end_date" id="facility_end_date" class="form-control datepicker" type="text" style="width:100%;" required data-date-format="dd-mm-yyyy" />
-							<span class="input-group-addon my-input-group-icon">
-								<i class="ace-icon fa fa-calendar"></i>
-							</span>
-						</div>
-						<i class="ace-icon fa fa-circle-o" style="position: absolute; right: 10px; top:20px;"></i>
-						<div style="margin-left:70%;">
-							<input type="submit" id="facility_generate_report" name="facility_generate_report" class="btn btn-primary" value="Generate Report" />
-						</div>
-					</form>
-				</div>
-				<!--Facility-->
-			</div>
+				<div class="hr hr8 hr-double"></div>
+			</div><!-- /.widget-main -->
+
+
+		</div><!-- /.widget-body -->
 	</div>
-	<!--Reports according to geographic area-->
-	
 </div>
-<div class="hr hr-18 hr-double dotted"></div>
+
