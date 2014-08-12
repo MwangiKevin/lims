f818dbb687ebf850b0b53a5c86dcda2a5e03d187
MwangiKevin <mwangikevinn@gmail.com>
2014-08-12 13:50:54 +0300

    UI
    Signed-off-by: MwangiKevin <mwangikevinn@gmail.com>

--- application/views/nav_itm_notifications_view.php
+++ application/views/nav_itm_notifications_view.php
@@ -1,4 +1,4 @@
-<a data-toggle="dropdown" class="dropdown-toggle" href="#">
+<a data-toggle="dropdown" class="dropdown-toggle" href="#" title="Notifications">
 	<i class="ace-icon fa fa-bell <?php  if(((int)sizeof($notifications))>0){echo "icon-animated-bell";}?>"></i>
 	<span class="badge badge-important"><?php echo sizeof($notifications);?></span>
 </a>
