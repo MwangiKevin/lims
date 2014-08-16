8ddf8bf9423add8eacd30c5e3efb4ecdec6f2eb5
Oscar Gichohi <shohiek@gmail.com>
2014-08-16 09:16:59 +0300

    Reports Module

--- application/modules/alerts/controllers/email.php
+++ application/modules/alerts/controllers/email.php
@@ -49,7 +49,7 @@ public function admin_mail()
 	function email($id, $recepient, $subject, $message)  
 	{
 		$time=date('Y-m-d');
-
+		$attached_file = $this->config->item("server_root")."downloads/doc1.pdf";
 		$config = array(
 			'protocol' => 'smtp',
 			'smtp_host' => 'ssl://smtp.googlemail.com',
@@ -61,15 +61,17 @@ public function admin_mail()
 		$this->load->library('email', $config);
 		$this->email->set_newline("\r\n");
 
-		$this->email->from('cd4lims.tz@gmail.com', 'CD4');
+		$this->email->from('cd4lims.tz@gmail.com', 'EID');
 		$this->email->to($recepient);
 		$this->email->subject($subject);
+		$this->email->attach($attached_file);
 		$this->email->message($message);
 
+		
 		if($this->email->send())
 			{	
 
-				$this->mail_model->send_mail($id, $recepient, $subject, $message, $time);
+				//$this->mail_model->send_mail($id, $recepient, $subject, $message, $time);
 				$this->admin_mail();
 			} else 
 			{
